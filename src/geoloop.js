49fcac193c7ca80c8ba9595391f6250b0cfcec66
Jonas Loos <33965649+JonasLoos@users.noreply.github.com>
2021-02-02 01:08:53 +0100

    Revert "try to fix loading", cuz not working
    This reverts commit 00db66075c9931d16e8b1e69c82bf0b229f9799a.

--- src/geoloop.js
+++ src/geoloop.js
@@ -102,7 +102,7 @@ export default class GeoLoop {
       // this is stupid to use setInterval.
       // but mapbox doesn't seem to have a on-source-loaded event that reliably works
       // for this purpose.
-      let attemptsLeft = 1;
+      let attemptsLeft = 10;
       const interval = setInterval(() => {
         if (!this.map) {
           console.log('map was unloaded while waiting for geo source');
@@ -116,7 +116,6 @@ export default class GeoLoop {
           attemptsLeft -= 1;
           if (attemptsLeft <= 0) {
             clearInterval(interval);
-            this.ctrl.loadGeo();
           }
         }
       }, 1000);
